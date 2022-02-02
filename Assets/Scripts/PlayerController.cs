8506d36d2aee717e9190849c0103aaa241c62a48
Kefi <57833470+Kefikhalil@users.noreply.github.com>
2022-02-02 23:30:05 +0100

    updated

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Exit");
+        }
+    }
+}
---
+++ Assets/Scripts/Textbox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Public class Textbox to activate the message
+/// </summary>
+public class Textbox : MonoBehaviour
+{
+    public GameObject info;
+
+    public void MoreInfo(GameObject info)
+    {
+        if (info.activeSelf == true)
+        {
+            info.SetActive(false);
+        }
+        else
+        {
+            info.SetActive(true);
+        }
+    }
+
+}
